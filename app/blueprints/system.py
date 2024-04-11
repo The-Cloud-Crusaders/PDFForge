5b537f85e13b34c970cecc645354d03164f02a58
monty16597 <alonjamohit77@gmail.com>
2024-04-11 17:38:46 -0400

    Added index.html

--- app/blueprints/system.py
+++ app/blueprints/system.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 system_bp = Blueprint("system", __name__)
 
 
 @system_bp.route("/", methods=["GET"])
 def index():
-    return "Use post request to /generate_pdf to generate a PDF file", 200
+    return render_template('index.html')
 
 
 @system_bp.route("/health", methods=["GET"])
