f2aa666cbfbfcccc8b85374b7beb4e655a628db0
monty16597 <alonjamohit77@gmail.com>
2024-04-13 15:40:45 -0400

    Fix pytest

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class AppConfig:
-    APP_AWS_S3_BUCKET_NAME = os.environ.get("APP_AWS_S3_BUCKET_NAME", None)
-    APP_AWS_ENDPOINT_URL = os.environ.get("APP_AWS_ENDPOINT_URL", None)
-    APP_AWS_ACCESS_KEY_ID = os.environ.get("APP_AWS_ACCESS_KEY_ID", None)
-    APP_AWS_SECRET_ACCESS_KEY = os.environ.get("APP_AWS_SECRET_ACCESS_KEY", None)
-    APP_AWS_REGION_NAME = os.environ.get("APP_AWS_REGION_NAME", "us-east-1")
--- tests/config.py
+++ tests/config.py
@@ -1,9 +1,9 @@
 import os
 
 
-class TestingConfig:
-    APP_AWS_S3_BUCKET_NAME = "test"
-    APP_AWS_ENDPOINT_URL = "http://localhost:4566"
-    APP_AWS_ACCESS_KEY_ID = "test"
-    APP_AWS_SECRET_ACCESS_KEY = "test"
-    APP_AWS_REGION_NAME = "us-east-1"
+class AppConfig:
+    APP_AWS_S3_BUCKET_NAME = os.getenv("APP_AWS_S3_BUCKET_NAME", "test")
+    APP_AWS_ENDPOINT_URL = os.getenv("APP_AWS_ENDPOINT_URL", None)
+    APP_AWS_ACCESS_KEY_ID = os.getenv("APP_AWS_ACCESS_KEY_ID", None)
+    APP_AWS_SECRET_ACCESS_KEY = os.getenv("APP_AWS_SECRET_ACCESS_KEY", None)
+    APP_AWS_REGION_NAME = os.getenv("APP_AWS_REGION_NAME", "us-east-1")
