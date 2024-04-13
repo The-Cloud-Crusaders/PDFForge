d15df75f469fb4994bf479db098baca5d4980c0c
monty16597 <alonjamohit77@gmail.com>
2024-04-13 15:26:03 -0400

    Removed debug true

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from app import create_app
 from config import AppConfig
 if __name__ == "__main__":
     app = create_app(AppConfig)
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host="0.0.0.0")
